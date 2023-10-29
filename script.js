fd850cb65f58e1fc95c7b62662161e1e6711e53c
Tandrios <t.andriessen@gmail.com>
2023-10-29 20:17:12 +0100

    Version 1

--- script.js
+++ script.js
@@ -14,9 +14,51 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function registerClick(e) {
-    const button = e.target.textContent
-    console.log(button);
-    if (button === 'AC') {
+    const button = e.target.textContent;
+
+    // If function key is pressed
+    if (e.target.classList.contains('function')) {
+        if (!num1 && !result && !operator && arrNumbers.length > 0) {
+            num1 = parseFloat(arrNumbers.join(''))
+            operator = button;
+            arrNumbers = [];
+        }
+        else if (!num1 && result && arrNumbers.length > 0 && !operator) {
+            operator = button;
+            num1 = result;
+            result = null
+            num2 = parseFloat(arrNumbers.join(''));
+            operate(operator, num1, num2)
+        } 
+        else if (!num1 && result && arrNumbers.length === 0 && !operator) {
+            operator = button;
+            num1 = result;
+            result = null
+        }
+        else if (num1 && !result && operator && arrNumbers.length > 0) {
+            num2 = parseFloat(arrNumbers.join(''));
+            operate(operator, num1, num2)
+        } 
+    }
+    // If number key is pressed
+    else if (e.target.classList.contains('number')) {
+        arrNumbers.push(button);
+        updateDisplay(arrNumbers.join(''));
+    }
+    // If compute key is pressed
+    else if (e.target.classList.contains('compute')) {
+        if (num1 && arrNumbers.length > 0) {
+            num2 = parseFloat(arrNumbers.join(''));
+            operate(operator, num1, num2)
+        } else if (result && arrNumbers.length > 0) {
+            num1 = result;
+            result = null
+            num2 = parseFloat(arrNumbers.join(''));
+            operate(operator, num1, num2)
+        }
+    }
+    // If AC key is pressed
+    else if (e.target.classList.contains('clear')) {
         num1 = null;
         num2 = null;
         operator = '';
@@ -24,52 +66,35 @@ function registerClick(e) {
         result = null;
         updateDisplay(0);
     }
-    // Check if the button is a number
-    if (!isNaN(button)) {
-        // Update the array with the number that is pressed
-        arrNumbers.push(button);
-        // Update the display
-        updateDisplay(arrNumbers.join(''));
-
-    // If the button is not a number        
-    } else if (isNaN(button)) {
-        if (button === '=') {
-            if (num1) {
-                num2 = parseFloat(arrNumbers.join(''));
-                operate(operator, num1, num2)
-            }
-        } else if (button === '.') {
-            // Update the array with the dot.
-            arrNumbers.push(button);
-            // Update the display
-            updateDisplay(arrNumbers.join(''));
-        } else {
-            if (result) {
-                operator = button;
-                num1 = result;
-                result = null
-                num2 = parseFloat(arrNumbers.join(''));
-                operate(operator, num1, num2)
-            } else if (!num1) {
-                num1 = parseFloat(arrNumbers.join(''))
-                operator = button;
-                arrNumbers = [];
-            } else if (num1 && !result) {
-                num2 = parseFloat(arrNumbers.join(''));
-                operate(operator, num1, num2)
-            } 
-        }
-    }
 }
 
 function operate (operator, n1, n2) {
     switch (operator) {
         case '+':
-            result = add(n1, n2)
-            updateDisplay(result)
+            result = add(n1, n2);
+            updateDisplay(result);
             clear();
             break;
-    
+        case '-':
+            result = subtract(n1, n2);
+            updateDisplay(result);
+            clear();
+            break;
+        case '*':
+            result = multiply(n1, n2);
+            updateDisplay(result);
+            clear();
+            break;
+        case '/':
+            result = divide(n1, n2);
+            updateDisplay(result);
+            clear();
+            break;
+        case '%':
+            result = divide(n1, n2);
+            updateDisplay(result);
+            clear();
+            break;
         default:
             break;
     }
@@ -91,9 +116,13 @@ function divide(n1, n2) {
     return n1 / n2;
 }
 
+function percentage(n1, n2) {
+    return n2 / n1 * 100;
+}
+
 function updateDisplay(numbers) {
     let displayText = document.querySelector('.display')
-    displayText.textContent = numbers
+    displayText.textContent = Math.round(numbers * 10000000) / 10000000;
     return numbers;
 }
 
